84de31eb6f2ec53aaea37dbaf0ec48148474b425
Victor Onyekwere <cydollarsign1@gmail.com>
2025-01-28 19:23:44 +0100

    Tasks Controllers

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -8,7 +8,7 @@ const asyncHandler = require("express-async-handler");
 // @access private
 const getAllUsers = asyncHandler(async (req, res) => {
   const users = await User.find().select("-password").lean();
-  if (!users) {
+  if (!users?.length) {
     return res.status(400).json({ message: "No Users Found" });
   }
   res.json(users);
@@ -56,22 +56,68 @@ const createnewUser = asyncHandler(async (req, res) => {
 // @route PATcH / user
 // @access private
 const updateUser = asyncHandler(async (req, res) => {
-    const {id, username, roles, active, password} = req.body
+  const { id, username, roles, active, password } = req.body;
 
-    if (!id || !username || !Array.isArray(roles) || !roles.length || typeof active !== "boolean") {
-        return res.status(400).json({message: "All fields are required"})
-    }
+  if (
+    !id ||
+    !username ||
+    !Array.isArray(roles) ||
+    !roles.length ||
+    typeof active !== "boolean"
+  ) {
+    return res.status(400).json({ message: "All fields are required" });
+  }
 
-    const user = await User.findById(id).exec()
+  const user = await User.findById(id).exec();
 
-    if (!user) {
-        return res.status(400).json({message: "User not found"})
-    }
+  if (!user) {
+    return res.status(400).json({ message: "User not found" });
+  }
+
+  //Avoid duplicate
+  const duplicate = await User.findOne({ username }).lean().exec();
+  if (duplicate && duplicate?._id.toString !== id) {
+    return res.status(409).json({ message: "Duplicate username" });
+  }
+
+  user.username = username;
+  user.roles = roles;
+  user.active = active;
+
+  if (password) {
+    user.password = await bcrypt.hash(password, 10);
+  }
+
+  const updatedUser = await user.save();
+
+  res.status(200).json({ message: `${updatedUser.username} updated` });
 });
 
 // @desc Delete a user
 // @route DELETE / user
 // @access private
-const deleteUser = asyncHandler(async (req, res) => {});
+const deleteUser = asyncHandler(async (req, res) => {
+  const { id } = req.body;
+  if (!id) {
+    return res.status(400).json({ message: "User ID required" });
+  }
+
+  const notes = await Note.findOne({ user: id }).lean().exec();
+  if (notes?.length) {
+    return res.status(400).json({message: "User has assigned notes"})
+  }
+
+  const user = await User.findById(id).exec()
+
+  if (!user) {
+    return res.status(400).json({message: "User not found"})
+  }
+
+  const result = user.deleteOne()
+
+  const reply = `Username ${result.username} with ID ${result.id} deleted`
+
+  res.json(reply)
+});
 
 module.exports = { getAllUsers, createnewUser, updateUser, deleteUser };
