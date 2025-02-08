9cb5cdc5ddacbf2ed24da9748f77c77620df3378
Victor Onyekwere <cydollarsign1@gmail.com>
2025-02-08 18:22:56 +0100

    Update

--- frontend/src/components/InputComponent.jsx
+++ frontend/src/components/InputComponent.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
 
-const InputComponent = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+const InputComponent = ({ labelText, type, id, name, ph, value, autocomplete, onchange, classes }) => {
+    return (
+        <>
+            <label className="form-label" htmlFor={id}>
+                {labelText}
+            </label>
+            <input
+                type={type}
+                id={id}
+                name={name}
+                placeholder={ph}
+                value={value}
+                autoComplete={autocomplete}
+                onChange={onchange}
+                className={`my-2 ${classes} border border-gray-200 py-2 px-4`} />
+        </>
+    )
 }
 
 export default InputComponent
--- frontend/src/features/tasks/EditTaskForm.jsx
+++ frontend/src/features/tasks/EditTaskForm.jsx
@@ -1,14 +1,66 @@
 import { useState } from 'react'
+import InputComponent from '../../components/InputComponent'
 
-const EditTaskForm = ({task, users}) => {
+const EditTaskForm = ({ task, users }) => {
   const [user, setUser] = useState(task.user)
-  const [title, seTitle] = useState(task.title)
+  const [title, setTitle] = useState(task.title)
   const [text, setText] = useState(task.text)
-  // const {title, text, user} = task
-  console.log(title, text, user, users)
+  const [listOfUsers, setListOfUsers] = useState("")
+
+  const handleTitleChange = (e) => {
+    setTitle(e.target.value)
+    console.log(e.target.value)
+  }
+
+  const handleDescChange = (e) => {
+    setText(e.target.value)
+  }
+  
+  const handleUserChange = (e) => {
+    // console.log(users, 1)
+  }
+
+  if (users) {
+    console.log("Users found")
+  }
+
   return (
-    <div>
-      
+    <div className='flex justify-center'>
+      <form className="flex flex-col w-1/2">
+        <InputComponent
+          labelText="Task Title:"
+          type="text"
+          id="taskTitle"
+          name="title"
+          ph="Enter a Task Title"
+          value={title}
+          autocomplete="off"
+          onchange={handleTitleChange}
+          classes="" />
+        <InputComponent
+          labelText="Task Description:"
+          type="text"
+          id="taskDesc"
+          name="text"
+          ph="Enter a Task Description"
+          value={text}
+          autocomplete="off"
+          onchange={handleDescChange}
+          classes="" />
+        <label className="form-label" htmlFor="users">
+          Assigned To:
+        </label>
+        <select
+          name="roles"
+          value={user}
+          className={`border overflow-hidden py-2 outline-none form-select`}
+          onChange={handleUserChange}
+          id="roles">
+            <option>Sammy</option>
+            <option>Sammy</option>
+            <option>Sammy</option>
+        </select>
+      </form>
     </div>
   )
 }
