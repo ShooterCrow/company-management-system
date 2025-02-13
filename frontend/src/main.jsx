53abbcb63ac29b516da826f7dfd58affa20a4a83
Victor Onyekwere <cydollarsign1@gmail.com>
2025-02-13 20:40:34 +0100

    Update

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,7 +5,10 @@ import App from './App.jsx'
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import { store } from "./app/store"
 import { Provider } from 'react-redux'
+import { disableReactDevTools } from "@disable-react-devtools"
 
+if (process.env.NODE_ENV === "production") disableReactDevTools()
+  
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
