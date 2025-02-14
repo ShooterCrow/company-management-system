c1a9028ed35eeaf2b260c40d5ea8f52aca3143b0
Victor Onyekwere <cydollarsign1@gmail.com>
2025-02-14 16:28:43 +0100

    API Update

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,7 +1,7 @@
 const allowedOrigins = [
     "https://company-management-system-03fp.onrender.com",
     "https://www.company-management-system-03fp.onrender.com",
-    "https://jj-companymng-api.onrender.com/"
+    "https://jj-companymng-api.onrender.com"
 ]
 
 module.exports = allowedOrigins
