823442810ddb9e060ac52fcff52bb61822692533
Victor Onyekwere <cydollarsign1@gmail.com>
2025-02-14 16:11:51 +0100

    API Update

--- frontend/src/app/api/apiSlice.js
+++ frontend/src/app/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
 import { setCredentials } from "../../features/auth/authSlice"
 
 const baseQuery = fetchBaseQuery ({
-    baseUrl: "https://jj-api.onrender.com",
+    baseUrl: "https://jj-companymng-api.onrender.com",
     credentials: "include",
     prepareHeaders: (headers, {getState}) => {
         const token = getState().auth.token
